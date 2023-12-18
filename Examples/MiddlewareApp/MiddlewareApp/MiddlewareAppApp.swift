9558b11559dced4e183e9c1b1413797b6c5ea039
Archish Thakkar <archish@middleware.io>
2023-12-18 16:19:19 +0530

    :art: added MiddlewareRum dependency

---
+++ Examples/MiddlewareApp/MiddlewareApp/MiddlewareAppApp.swift
@@ -0,0 +1,29 @@
+//
+//  MiddlewareAppApp.swift
+//  MiddlewareApp
+//
+//  Created by Archish Thakkar on 12/18/23.
+//
+
+import SwiftUI
+//import middlewarerum
+import MiddlewareRum
+
+@main
+struct MiddlewareAppApp: App {
+    init() {
+//        MiddlwareRumBuilder
+//            .target("https://demo.middleware.io")
+//            .serviceName("Mobile-SDK-ios")
+//            .projectName("Mobile-SDK-ios")
+//            .rumAccessToken("sometoken")
+//            .deploymentEnvironment("PROD")
+//            .globalAttributes()
+//            .build()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Sources/MiddlewareRum/MiddlewareRum.swift
+++ Sources/MiddlewareRum/MiddlewareRum.swift
@@ -1,7 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Foundation
+import OpenTelemetryApi
 
 public class MiddlewareRum: NSObject {
-    
+    public override init() {
+        
+    }
 }
