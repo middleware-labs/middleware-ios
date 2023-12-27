513b3800a9bb1f61120476731c52fd8d4b8f978a
Archish Thakkar <archish@middleware.io>
2023-12-27 20:48:26 +0530

    :art: support for tvOS

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "middleware-ios",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v10_15),
+        .tvOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
